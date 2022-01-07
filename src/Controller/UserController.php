77b125a50642bbb3fa290f6320dc6e5a58aef6de
Shyhyro <Bosquet.laurie@outlook.com>
2022-01-07 16:43:11 +0100

    create article

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,4 +20,15 @@ class UserController extends Controller
             echo $e->getMessage();
         }
     }
+
+    public function add()
+    {
+        $user = strip_tags(trim($_POST['user']));
+
+        $article = (new UserManager())->addUser($user);
+
+        if ($article) {
+            header('location:index.php?controller=user');
+        }
+    }
 }
